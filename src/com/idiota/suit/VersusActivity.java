04c2947cc308d5490e15b702a20fb157b9795c42
ninoaquinas <ninoaquinas@gmail.com>
2014-02-16 14:50:18 -0800

    adding versus activity and the basic logic

--- src/com/idiota/suit/VersusActivity.java
+++ src/com/idiota/suit/VersusActivity.java
@@ -2,7 +2,12 @@ package com.idiota.suit;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class VersusActivity extends Activity {
 
@@ -10,6 +15,44 @@ public class VersusActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_versus);
+		
+		Intent intent = getIntent();
+		String friendName = intent.getStringExtra("name");
+		String friendPhoto = intent.getStringExtra("photo");
+		int friendId = intent.getIntExtra("id", 0);
+		
+		//set all the content of view
+
+		ImageView friendPhotoView = (ImageView) findViewById(R.id.versus_friend_image);
+		
+		TextView friendNameView = (TextView) findViewById(R.id.versus_friend_name);
+		friendNameView.setText(friendName);
+		
+		//friend information depends on friend condition
+		String friendInformation = "Belom maen suit nih dia :s";
+		TextView friendInformationView = (TextView) findViewById(R.id.versus_friend_information);
+		friendInformationView.setText(friendInformation);
+		
+		//match information, hide the button if there is still outstanding challenge
+		TextView matchInformationView = (TextView) findViewById(R.id.versus_match_information);
+		matchInformationView.setVisibility(View.GONE);
+		Button matchButtonView = (Button) findViewById(R.id.versus_button);
+		
+		//win lose stats data will be retrieved from parse
+		int win = 100;
+		int draw = 2;
+		int lose = 40;
+		TextView versusTotalKalahView = (TextView) findViewById(R.id.versus_total_kalah);
+		versusTotalKalahView.setText(Integer.toString(lose));
+
+		TextView versusTotalMenangView = (TextView) findViewById(R.id.versus_total_menang);
+		versusTotalMenangView.setText(Integer.toString(win));
+		
+		TextView versusTotalSeriView = (TextView) findViewById(R.id.versus_total_seri);
+		versusTotalSeriView.setText(Integer.toString(draw));
+		
+		
+		
 	}
 
 	@Override
